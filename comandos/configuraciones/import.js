97bd289ec4126de600e736164aa87baf8b945c07
Félix Ofx <xlfelixgamer@gmail.com>
2026-02-04 19:07:23 -0400

    Actualizar import.js

--- comandos/configuraciones/import.js
+++ comandos/configuraciones/import.js
@@ -1,7 +1,7 @@
 import manejador.js from './manejador.js'
 
-const WORKER_DIR = Helper.__dirname(import.meta.url, false)
-    module = Helper.__filename(module)
+const WORKER_DIR = manejador.__dirname(import.meta.url, false)
+    module = manejador.__filename(module)
     const module_ = await import(`${module}?id=${Date.now()}`)
     const result = module_ && 'default' in module_ ? module_.default : module_
     return result
